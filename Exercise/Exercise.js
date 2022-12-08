e226b6da5b4f5c9f22f97d6615eacf719f73223e
Radosław Milczarek <116893317+RadoRadoslaw@users.noreply.github.com>
2022-12-08 15:33:27 +0100

    yyyy

--- Exercise/Exercise.js
+++ Exercise/Exercise.js
@@ -8,3 +8,15 @@ $(".dropdown-select").on("click",function(){
   }
 });
 
+$(".dropdown-list a").on("click",function(){
+  $(".dropdown-list").removeClass("active");
+
+  let href=$(this).attr("data-href");
+  let elem=$(href);
+  if(elem.hasClass("active")==true){
+    elem.removeClass("active");
+  }else{
+    elem.addClass("active");
+  }
+});
+
