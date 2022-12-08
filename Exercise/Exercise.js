d30a25648e8bd9ad2b40697c8f15129e53ff8bac
Radosław Milczarek <116893317+RadoRadoslaw@users.noreply.github.com>
2022-12-08 01:52:06 +0100

    zmiany

---
+++ Exercise/Exercise.js
@@ -0,0 +1,21 @@
+// function update() {
+//     const l = game.objects;
+//     for (let i = 0; i < l.length; i++) {
+//       const e = l[i];
+//       e.old.x = e.x;
+//       e.old.y = e.y;
+//       e.update();
+//       e.render();
+//       if (e.destroy) {
+//         e.element.remove();
+//         l.splice(i, 1);
+//         i--;
+//       }
+//     }
+  
+//     requestAnimationFrame(update);
+//   }
+//   update();
+  
+  const podciąganie = document.querySelector("a1");
+  console.log(podciąganie);
