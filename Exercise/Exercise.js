f603a675287594bc3b70901a7ab547a77b44a469
Radosław Milczarek <116893317+RadoRadoslaw@users.noreply.github.com>
2022-12-12 12:38:11 +0100

    Zmiana

--- Exercise/Exercise.js
+++ Exercise/Exercise.js
@@ -22,5 +22,15 @@ $(".dropdown-list a").on("click",function(){
 });
 
 
+// $("#pull-up").on("click",function(){
+//   $(".dropdown-list").removeClass("active");
 
+//   let href=$(this).attr("data-href");
+//   let elem=$(href);
+//   if(elem.hasClass("active")==true){
+//     elem.removeClass("active");
+//   }else{
+//     elem.addClass("active");
+//   }
+// });
 
