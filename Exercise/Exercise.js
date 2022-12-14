ca93b9a3fceb0efc28c32f78b7e3deb0264c92c9
Radosław Milczarek <116893317+RadoRadoslaw@users.noreply.github.com>
2022-12-14 21:57:25 +0100

    KALKU

--- Exercise/Exercise.js
+++ Exercise/Exercise.js
@@ -19,8 +19,32 @@ $(".dropdown-list a").on("click",function(){
     elem.addClass("active");
   }
 });
+ 
+// KALKULATOR:
+let add = document.getElementById("increment");
 
+let remove = document.getElementById("decrement");
 
+let int = document.getElementById("number");
+
+let integer = 0;
+
+add.addEventListener("click", function(){
+  integer += 1;
+  int.innerHTML = integer;
+});
+
+remove.addEventListener("click", function(){
+  integer -= 1;
+  int.innerHTML = integer;
+});
+
+let done = document.getElementById("done");
+
+done.addEventListener("click", function(){
+  integer = 0
+  int.innerHTML = integer;
+});
 
 
 
