e6a23e91061a494288a9d7515e7ea08d7d213c9d
Radosław Milczarek <116893317+RadoRadoslaw@users.noreply.github.com>
2022-12-23 15:14:29 +0100

    kalku 4

--- Exercise/Exercise.js
+++ Exercise/Exercise.js
@@ -30,33 +30,68 @@ let integer = 0;
 
 $("#kalku1 #increment").on("click", function(){
   integer += 1;
-  $("#kalku1 #number").html(integer);
+  $("#kalku1 .number").html(integer);
 });
 
 $("#kalku1 #decrement").on("click", function(){
   integer -= 1;
-  $("#kalku1 #number").html(integer);
+  $("#kalku1 .number").html(integer);
 });
 
 $("#kalku2 #increment").on("click", function(){
   integer += 1;
-  $("#kalku2 #number").html(integer);
+  $("#kalku2 .number").html(integer);
 });
 
 $("#kalku2 #decrement").on("click", function(){
   integer -= 1;
-  $("#kalku2 #number").html(integer);
+  $("#kalku2 .number").html(integer);
 });
 
 $("#kalku3 #increment").on("click", function(){
   integer += 1;
-  $("#kalku3 #number").html(integer);
+  $("#kalku3 .number").html(integer);
 });
 
 $("#kalku3 #decrement").on("click", function(){
   integer -= 1;
-  $("#kalku3 #number").html(integer);
+  $("#kalku3 .number").html(integer);
 });
+$("#kalku4 #increment").on("click", function(){
+  integer += 1;
+  $("#kalku4 .number").html(integer);
+});
+
+$("#kalku4 #decrement").on("click", function(){
+  integer -= 1;
+  $("#kalku4 .number").html(integer);
+});
+
+////////////dodawanie ćwiczenia/////////////
+let EXER = ""
+
+
+const add=localStorage.getItem("name");
+
+if(load){
+	EXER=JSON.parse(load);
+	$("#other").html(" "+EXER);
+}
+
+$("#additional  #add").on("click", function(){
+  EXER.push("#addexercise");
+  $("#other").html(""+EXER);
+  localStorage.setItem("name",JSON.stringify(EXER));
+  // "#other" = "";
+});
+
+// if("#additional  #add").on("click", function(){
+//    $("#kalku4")
+//     (elem.hasClass("show")==true)
+// });
+  
+
+
 // WRZUCANIE DO SETA:
 
 // WCZYTYWANIE DANYCH DO SETS:
@@ -74,10 +109,6 @@ $("#kalku1  #done").on("click", function(){
   integer = 0;
 });
 
-
-
-
-
 ///////////kalku2//////
 let SETS2 = []
 const load2=localStorage.getItem("sets2");
@@ -93,9 +124,6 @@ $("#kalku2  #done").on("click", function(){
    integer = 0;
 });
 
-
-
-
 //////////////////kalku3///////////////
 let SETS3= []
 const load3=localStorage.getItem("sets3");
@@ -109,6 +137,20 @@ $("#kalku3  #done").on("click", function(){
   $("#sets3").html("Your sets: "+SETS3);
   localStorage.setItem("sets3",JSON.stringify(SETS3));
    integer = 0;
+///////////////////////////////////////////////////////////
+});
+let SETS4= []
+const load4=localStorage.getItem("sets4");
+if(load){
+	SETS4=JSON.parse(load);
+	$("#sets4").html("Your sets: "+SETS3);
+}
+
+$("#kalku4  #done").on("click", function(){
+  SETS4.push(integer);
+  $("#sets4").html("Your sets: "+SETS4);
+  localStorage.setItem("sets4",JSON.stringify(SETS4));
+   integer = 0;
 
 });
 
@@ -117,10 +159,12 @@ $(".kalku  #reset").on("click", function(){
   SETS=[]
   SETS2=[]
   SETS3=[]
-$(".kalku #number").html(integer=0)
+  SETS4=[]
+$(".kalku .number").html(integer=0)
 $(".kalku #sets").html("Your sets: ")
 $(".kalku #sets2").html("Your sets: ")
 $(".kalku #sets3").html("Your sets: ")
+$(".kalku #sets4").html("Your sets: ")
   
 });
 
